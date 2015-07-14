8406e01df489f9b2a7d8dd10cf62aeb14b160a19
wesyah234 <wesyah234@users.noreply.github.com>
2015-07-14 13:39:57 -0500

    clean up a bit

--- quickformdemo.js
+++ quickformdemo.js
@@ -8,35 +8,39 @@ Books.attachSchema(new SimpleSchema({
   author: {
     type: String,
     label: "Author"
-  },  
-  'nestedFieldMultiple' : {
-    type:[Object],
-    label:"multiple city state blocks below"
   },
-  'nestedFieldMultiple.$.city': {
+  extra: {
     type: String,
-    label: "City",
-    max: 200
+    label: "Extra"
   },
-  'nestedFieldMultiple.$.state': {
-    type: String,
-    label: "State",
-    max: 200
-  },
-  'nestedField' : {
-    type:Object,
-    label:"singly occurring nested field"
-  },
-  'nestedField.city': {
-    type: String,
-    label: "City",
-    max: 200
-  },
-  'nestedField.state': {
-    type: String,
-    label: "State",
-    max: 200
-  }
+//  'nestedFieldMultiple' : {
+//    type:[Object],
+//    label:"multiple city state blocks below"
+//  },
+//  'nestedFieldMultiple.$.city': {
+//    type: String,
+//    label: "City",
+//    max: 200
+//  },
+//  'nestedFieldMultiple.$.state': {
+//    type: String,
+//    label: "State",
+//    max: 200
+//  },
+//  'nestedField' : {
+//    type:Object,
+//    label:"singly occurring nested field"
+//  },
+//  'nestedField.city': {
+//    type: String,
+//    label: "City",
+//    max: 200
+//  },
+//  'nestedField.state': {
+//    type: String,
+//    label: "State",
+//    max: 200
+//  }
 }));
 
 
@@ -45,18 +49,36 @@ Books.attachSchema(new SimpleSchema({
 Router.configure({
   layoutTemplate: 'main'
 });
-Router.route('/', 'insertAndShowBooks');
+Router.route('/', 'showBooks');
+Router.route('/new', 'insertBook');
 Router.route('/book/:_id', function () {
+  Meteor.subscribe('book', this.params._id);
   var item = Books.findOne({_id: this.params._id});
   this.render('editBook', {data: item});
 }, {
   name: 'book.show'
+});
+Router.route('/deleteBook/:_id', function () {
+  Meteor.call("deleteBook", this.params._id);
+  Router.go('/');
+}, {
+  name: 'book.delete'
 });
 
 Meteor.methods({
   insertBook: function (theBook) {
     console.log('inserting book ', theBook);
     Books.insert(theBook);
+    if (Meteor.isClient) {
+      Router.go('/');
+    }
+  },
+  deleteBook: function (bookId) {
+    console.log('deleting book ', bookId);
+    Books.remove({_id: bookId});
+    if (Meteor.isClient) {
+      Router.go('/');
+    }
   },
   updateBook: function (modifier, id) {
     console.log("updateBook method running updating these fields", modifier);
@@ -70,8 +92,9 @@ Meteor.methods({
 
 if (Meteor.isClient) {
   Meteor.subscribe('books');
-  Template.insertAndShowBooks.helpers({
+  Template.showBooks.helpers({
     'theBooks': function () {
+      console.log("finding books in the helper");
       return Books.find({});
     }
   });
@@ -79,7 +102,13 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.publish("books", function () {
-    var foundOnServer = Books.find({});
+    var foundOnServer = Books.find({}, {sort: {"title": 1}, fields: {"_id": 1, "title": 1, "author": 1 }});
+    console.log("returning books")
+    return foundOnServer;
+  });
+  Meteor.publish("book", function (id) {
+    var foundOnServer = Books.find({_id:id}, {fields: {"_id": 1, "title": 1, "author": 1, "extra":1 }});
+    console.log("returning a single book with full info, id: " + id);
     return foundOnServer;
   });
 }
