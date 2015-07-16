5dc67088ec1b6b61fb98c6ad1dfafead0ca35e10
wesyah234 <wesyah234@users.noreply.github.com>
2015-07-16 14:29:04 -0500

    add the action/waitOn stuff

--- quickformdemo.js
+++ quickformdemo.js
@@ -27,15 +27,15 @@ Books.attachSchema(new SimpleSchema({
 //    label: "State",
 //    max: 200
 //  },
-//  'nestedField' : {
-//    type:Object,
-//    label:"singly occurring nested field"
-//  },
-//  'nestedField.city': {
-//    type: String,
-//    label: "City",
-//    max: 200
-//  },
+  'nestedField' : {
+    type:Object,
+    label:"singly occurring nested field"
+  },
+  'nestedField.city': {
+    type: String,
+    label: "City",
+    max: 200
+  },
 //  'nestedField.state': {
 //    type: String,
 //    label: "State",
@@ -48,20 +48,32 @@ subs = new SubsManager();
 Router.configure({
   layoutTemplate: 'main'
 });
-Router.route('/', function () {
-  subs.subscribe('books');
-  this.render('showBooks');
-}, {
-  name: 'home'
+Router.route('/', {
+  action: function () {
+    console.log("root action...");
+    this.render('showBooks');
+  },
+  name: 'home',
+  waitOn: function () {
+    console.log("root waitOn...");
+    subs.subscribe('books');
+  }
 });
 Router.route('/new', 'insertBook');
-Router.route('/book/:_id', function () {
-  subs.subscribe('book', this.params._id);
-  var item = Books.findOne({_id: this.params._id});
-  this.render('editBook', {data: item});
-}, {
-  name: 'book.show'
-});
+Router.route('/book/:_id', {
+  action: function () {
+    console.log("/book/:id action, finding the book " + this.params._id);
+    var item = Books.findOne({_id: this.params._id});
+    console.log("/book/:id action, found book:", item);
+    this.render('editBook', {data: item});
+  },
+  name: 'book.show',
+  waitOn: function () {
+    console.log("/book/:id waitOn...");
+    subs.subscribe('book', this.params._id);
+  }
+}
+);
 Router.route('/deleteBook/:_id', function () {
   Meteor.call("deleteBook", this.params._id);
   Router.go('/');
@@ -97,8 +109,10 @@ Meteor.methods({
 if (Meteor.isClient) {
   Template.showBooks.helpers({
     'theBooks': function () {
-      console.log("finding books in the helper");
-      return Books.find({});
+      console.log("showBooks theBooks helper doing a find of all books...");
+      var books = Books.find({});
+      console.log("got em");
+      return books;
     }
   });
 
@@ -106,12 +120,16 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.publish("books", function () {
+//    console.log("fetching books, but it takes 6  seconds...")
+//    Meteor._sleepForMs(6000);
     var foundOnServer = Books.find({}, {sort: {"title": 1}, fields: {"_id": 1, "title": 1, "author": 1}});
     console.log("returning books")
     return foundOnServer;
   });
   Meteor.publish("book", function (id) {
-    var foundOnServer = Books.find({_id: id}, {fields: {"_id": 1, "title": 1, "author": 1, "extra": 1}});
+//    console.log("fetching book " + id + ", waiting 4 sec...")
+//    Meteor._sleepForMs(4000);
+    var foundOnServer = Books.find({_id: id}, {fields: {"_id": 1, "title": 1, "author": 1, "extra": 1, "nestedField":1}});
     console.log("returning a single book with full info, id: " + id);
     return foundOnServer;
   });
